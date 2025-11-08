2a541e5b0bd4e8d24e89c768887a53587eace5f1
MartinkaV2 <medveczmartin@gmail.com>
2025-11-08 14:44:43 +0100

    Verzió 8.1
    Range fixelve, main menu elkezdve

--- Desktop/Assets/_Scripts/Tower/TowerPlacement.cs
+++ Desktop/Assets/_Scripts/Tower/TowerPlacement.cs
@@ -12,9 +12,13 @@ public class TowerPlacement : MonoBehaviour
 
     [NonSerialized] public bool isPlacing = true;
     private bool isRestricted = false;
+    private int overlappingTowers = 0;
+
     void Awake()
     {
-     rangeCollider.enabled = false; 
+        // Ne kapcsoljuk ki a rangeCollider-t, csak állítsuk be a sprite láthatóságát
+        if (rangeSprite != null)
+            rangeSprite.enabled = true;
     }
 
     void Update()
@@ -22,40 +26,70 @@ public class TowerPlacement : MonoBehaviour
         if (isPlacing)
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
             transform.position = mousePosition;
+
+            // Range sprite mindig látszik placement közben
+            if (rangeSprite != null)
+                rangeSprite.enabled = true;
         }
 
-        if(Input.GetMouseButtonDown(1) && !isRestricted)
+        if (Input.GetMouseButtonDown(1) && !isRestricted && overlappingTowers == 0)
         {
-            rangeCollider.enabled = true;
+            // Placement befejezése - csak a sprite-ot rejtjük el
+            if (rangeSprite != null)
+                rangeSprite.enabled = false;
+
             isPlacing = false;
-            GetComponent<TowerPlacement>().enabled = false;
+            // Ne kapcsoljuk ki a TowerPlacement komponenst, mert akkor a trigger-ek nem mûködnek
+            // GetComponent<TowerPlacement>().enabled = false;
+
+            // Ehelyett csak állítsuk be, hogy ne legyen placing módban
+            this.enabled = false;
         }
 
-        if (isRestricted)
+        // Szín frissítése placement közben
+        if (isPlacing && rangeSprite != null)
         {
-            rangeSprite.color = red;
-        }
-        else 
-        {
-            rangeSprite.color = gray;
+            if (isRestricted || overlappingTowers > 0)
+            {
+                rangeSprite.color = red;
+            }
+            else
+            {
+                rangeSprite.color = gray;
+            }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Restricted" || collision.gameObject.tag == "Tower" && isPlacing)
+        if (!isPlacing) return;
+
+        if (collision.gameObject.tag == "Restricted")
         {
             isRestricted = true;
+        }
+
+        // Ellenõrizzük, hogy más torony van-e a közelben
+        if (collision.gameObject.tag == "Tower" && collision.gameObject != this.gameObject)
+        {
+            overlappingTowers++;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Restricted" || collision.gameObject.tag == "Tower" && isPlacing)
+        if (!isPlacing) return;
+
+        if (collision.gameObject.tag == "Restricted")
         {
             isRestricted = false;
+        }
+
+        // Csökkentjük a számlálót, ha egy torony elhagyja a területet
+        if (collision.gameObject.tag == "Tower" && collision.gameObject != this.gameObject)
+        {
+            overlappingTowers--;
         }
     }
 }
