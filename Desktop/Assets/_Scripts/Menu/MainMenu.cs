e312a39f4164143e64cc51c3b0d28944d1e90769
MartinkaV2 <medveczmartin@gmail.com>
2025-11-09 12:57:56 +0100

    Verzió 9
    Bekerült a Pause Menu és a Main menu

--- Desktop/Assets/_Scripts/Menu/MainMenu.cs
+++ Desktop/Assets/_Scripts/Menu/MainMenu.cs
@@ -9,6 +9,12 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+        Time.timeScale = 1; 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
 }
