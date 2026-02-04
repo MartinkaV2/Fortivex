3dcbc746e2ba033b9d53fc10e53da51bf949084c
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2026-02-04 12:43:39 +0100

    Add files via upload
    Mai backend fejlesztés/javítás. Működik a deployment, 500as CORS hiba eltűnt.

---
+++ Backend/FortivexAPI0204WorkingDeploy/Dtos/PlayerProgressDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FortivexAPI.Dtos
+{
+    public class PlayerProgressDto
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int? BestTime { get; set; }
+    }
+}
