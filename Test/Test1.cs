7a74879ccea5209d51382ef5c74d195bd6e1f307
vighgreta <gretu.vigh99@gmail.com>
2026-04-18 18:38:20 +0200

    MSTeszt1

---
+++ Test/Test1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FortivexAPI.Controllers;
+using FortivexAPI.Data;
+using FortivexAPI.Helpers;
+using FortivexAPI.Dtos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace FortivexAPI.Tests.MsTest
+{
+    [TestClass] // xUnit-nál nincs ilyen, MSTest-nél kötelező
+    public class AccountsMsTests
+    {
+        private FortivexContext GetContext()
+        {
+            var options = new DbContextOptionsBuilder<FortivexContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var context = new FortivexContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+        private JwtHelper GetJwt()
+        {
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string> {
+                    {"Jwt:Key", "SuperSecretMsTestKey1234567890123456"},
+                    {"Jwt:Issuer", "Test"},
+                    {"Jwt:Audience", "Test"}
+                }).Build();
+            return new JwtHelper(config);
+        }
+
+        [TestMethod] // xUnit [Fact] megfelelője
+        public async Task GetAccounts_ReturnsOk()
+        {
+            // Arrange
+            using var context = GetContext();
+            var controller = new AccountsController(context, GetJwt());
+
+            // Act
+            var result = await controller.GetAccounts();
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+        }
+    }
+}
